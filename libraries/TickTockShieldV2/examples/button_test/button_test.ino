83dc0548af59453c3f84d4bb93af18b77c00df03
reeedstudio <luweicong@qq.com>
2013-11-22 10:42:32 +0800

    light sensor ok

--- libraries/TickTockShieldV2/examples/button_test/button_test.ino
+++ libraries/TickTockShieldV2/examples/button_test/button_test.ino
@@ -1,17 +1,37 @@
 // test button
 
-#include <Streaming.h>
-#include <TTSButton.h>
 #include <Wire.h>
+#include <Streaming.h>
+
+#include <TTSButton.h>
+#include <TTSBuzzer.h>
+#include <TTSLight.h>
+
 
 TTSButton button1(TTSK1);
 TTSButton button2(TTSK2);
 TTSButton button3(TTSK3);
 
+TTSBuzzer buz;
+
+TTSLight light;
+
+
+int state = 0;
+
 void setup()
 {
     Serial.begin(115200);
     cout << "hello world" << endl;
+    
+    cout << light.get() << endl;
+    
+    
+    while(1)
+    {
+        cout << analogRead(A1) << endl;
+        delay(100);
+    }
 }
 
 void loop()
@@ -22,6 +42,13 @@ void loop()
         if(button1.pressed())
         {
             cout << "button1 pressed" << endl;
+            
+            state = 1-state;
+            
+            if(state)buz.on();
+            else buz.off();
+            
+            cout << light.get() << endl;
         }
         
         while(button1.pressed());
